1ea97c9b897c2071e491fc0486984ad797ccf1b6
Tetrergeru <41305740+Tetrergeru@users.noreply.github.com>
2020-11-20 00:24:43 +0300

    Add code

---
+++ ai-4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+using CLIPSNET;
+
+namespace GraphFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var _form = new Form();
+            Application.Run(_form);
+        }
+    }
+}
